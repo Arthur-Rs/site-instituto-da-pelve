449bae8ca2f952cf4bfddbb18798187b3100eb9f
Arthur-Rs <arthurreis074@gmail.com>
2020-11-05 20:56:04 -0300

    build: 2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,54 @@
+//#region Variables
+
 const btnHeader = document.querySelector('#btn-header') as Element
 const header = document.querySelector('header') as Element
 const main = document.querySelector('main') as HTMLDivElement
 
+const HeaderButtons = document.querySelectorAll<HTMLSpanElement>('header nav span') 
 
+const Sections = {
+  home: document.querySelector('.home') as HTMLDivElement,
+  about: document.querySelector('.about') as HTMLDivElement,
+  matter: document.querySelector('.matter') as HTMLDivElement,
+}
 
+type keysSections = 'home' | 'about' | 'matter'
 
-btnHeader.addEventListener('click', () => {
+const offsetHeight = Sections.home.offsetTop
+
+//#endregion
+
+//#region Functions
+
+const toggleHeader = () => {
   btnHeader.classList.toggle('close')
   header.classList.toggle('open')
   main.classList.toggle('header-open')
+}
+
+const scrollToSection = (sectionName:keysSections ) => {
+  const Section = Sections[sectionName]
+  const positionSection = Section.offsetTop - offsetHeight
+
+  scrollTo({
+    top: positionSection,
+    behavior: 'smooth'
+
+  })
+
+  toggleHeader()
+}
+
+//#endregion
+
+//#region Events
+
+HeaderButtons.forEach((button)=>{
+  button.addEventListener('click', () => {
+    scrollToSection(button.dataset.link as keysSections)
+  })
 })
+
+btnHeader.addEventListener('click', toggleHeader)
+
+//#endregion
