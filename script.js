68742093f0cb82039c2024427c892e64ad44ee33
Arthur-Rs <arthurreis074@gmail.com>
2020-11-05 19:13:06 -0300

    Updates

---
+++ script.js
@@ -0,0 +1,1 @@
+"use strict";var btnHeader=document.querySelector("#btn-header"),header=document.querySelector("header"),main=document.querySelector("main");btnHeader.addEventListener("click",function(){btnHeader.classList.toggle("close"),header.classList.toggle("open"),main.classList.toggle("header-open")});
